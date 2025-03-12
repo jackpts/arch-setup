dfe2aa1a58e672f47006acfde1eaa00a68ed6aa4
Jack Pts <jackpts1981@gmail.com>
2025-03-12 18:44:23 +0300

    [arch-setup] yay w/ paru replacement

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# yay install
 sudo pacman -S --needed base-devel git
-git clone https://aur.archlinux.org/yay.git ~/Downloads
-cd ~/Downloads/yay
-makepkg -si
+
+# yay install
+# git clone https://aur.archlinux.org/yay.git ~/Downloads
+# cd ~/Downloads/yay
+# makepkg -si
 
 # paru install
 git clone https://aur.archlinux.org/paru.git ~/Downloads
@@ -12,4 +13,4 @@ cd ~/Downloads/paru
 makepkg -si
 
 ./pkg_install.sh pacman.txt pacman
-./pkg_install.sh yay.txt yay
+./pkg_install.sh paru.txt paru
