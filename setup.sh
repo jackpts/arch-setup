8d29a610273fe5c88058d114b9a4b73fdd18d680
Jack Pts <jackpts1981@gmail.com>
2025-03-12 18:49:19 +0300

    [arch-setup] update setup w/ dotfiles installation

--- setup.sh
+++ setup.sh
@@ -12,5 +12,19 @@ git clone https://aur.archlinux.org/paru.git ~/Downloads
 cd ~/Downloads/paru
 makepkg -si
 
+# Install base packages
 ./pkg_install.sh pacman.txt pacman
 ./pkg_install.sh paru.txt paru
+
+# Install dotfiles packages
+sudo pacman -S stow
+git clone --depth 1 https://github.com/jackpts/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+stow .
+
+# Install Hyprshot UI
+git clone https://github.com/s-adi-dev/hyprshot-gui.git ~/Downloads
+cd ~/Downloads/hyprshot-gui
+./install.sh
+
+
